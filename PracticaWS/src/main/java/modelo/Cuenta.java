392f2e800aa3fd2513ecf11e0bdcc4f2ba1b1576
jcall <jcall@DELL-JOSE>
2020-01-16 12:13:43 -0500

    cuenta

--- PracticaWS/src/main/java/modelo/Cuenta.java
+++ PracticaWS/src/main/java/modelo/Cuenta.java
@@ -1,5 +1,25 @@
 package modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cuenta {
+	
+	@Id
+	@Column(name = "ws_cu_id")
+	private int id;
+	
+	@Column(name = "ws_cu_nombre")
+	private String nombre;
+	
+	@Column(name = "ws_cu_apellido")
+	private String apellido;
+	
+	
+	
 
 }
