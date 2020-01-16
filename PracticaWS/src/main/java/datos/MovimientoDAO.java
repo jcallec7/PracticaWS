be7b8b2ae1c33ef9b988533dce1a7cef46a77cd7
pedro <pedro@LAPTOP-ILA641ID>
2020-01-16 12:20:00 -0500

    Avance Movimiento

--- PracticaWS/src/main/java/datos/MovimientoDAO.java
+++ PracticaWS/src/main/java/datos/MovimientoDAO.java
@@ -1,5 +1,60 @@
 package datos;
 
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import modelo.Movimiento;
+
+@Stateless
 public class MovimientoDAO {
+
+	@Inject
+	private EntityManager em;
+
+	public void insert(Movimiento movimiento) {
+		em.persist(movimiento);
+
+	}
+
+	public void update(Movimiento movimiento) {
+		em.merge(movimiento);
+	}
+
+	public void remove(int id) {
+
+		Movimiento movimiento = this.read(id);
+		em.remove(movimiento);
+
+	}
+
+	public Movimiento read(int id) {
+
+		Movimiento a = em.find(Movimiento.class, id);
+		return a;
+
+	}
+
+	public List<Movimiento> getMovimientos() {
+
+		String jpql = "SELECT c FROM Movimiento c";
+		Query q = em.createQuery(jpql, Movimiento.class);
+
+		List<Movimiento> movimientos = q.getResultList();
+
+		return movimientos;
+	}
+
+	public List<Movimiento> getMovimientosPorCuenta(int filtro) {
+		String jpql = "SELECT a FROM Movimiento a WHERE ws_cu_id = ?1";
+		Query q = em.createQuery(jpql, Movimiento.class);
+		q.setParameter(1, "%" + filtro + "%");
+
+		List<Movimiento> movimientos = q.getResultList();
+
+		return movimientos;
+	}
 
 }
--- PracticaWS/src/main/java/modelo/Movimiento.java
+++ PracticaWS/src/main/java/modelo/Movimiento.java
@@ -1,12 +1,18 @@
 package modelo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Movimiento implements Serializable {
@@ -17,5 +23,54 @@ public class Movimiento implements Serializable {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name = "ws_mov_id")
 	private int id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "ws_mov_fecha")
+	private Date fecha;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "ws_ori_id")
+	private Cuenta origen;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ws_dest_id")
+	private Cuenta destino;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Cuenta getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(Cuenta origen) {
+		this.origen = origen;
+	}
+
+	public Cuenta getDestino() {
+		return destino;
+	}
+
+	public void setDestino(Cuenta destino) {
+		this.destino = destino;
+	}
+
+	@Override
+	public String toString() {
+		return "Movimiento [id=" + id + ", fecha=" + fecha + ", origen=" + origen + ", destino=" + destino + "]";
+	}
 	
 }
