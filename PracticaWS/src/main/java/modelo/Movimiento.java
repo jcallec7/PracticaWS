7d818f4aeca3f6d67421c159f7fd1a44e2604561
pedro <pedro@LAPTOP-ILA641ID>
2020-01-16 12:06:12 -0500

    Corregido

--- PracticaWS/src/main/java/modelo/Movimiento.java
+++ PracticaWS/src/main/java/modelo/Movimiento.java
@@ -1,10 +1,21 @@
 package modelo;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-public class Movimiento {
+public class Movimiento implements Serializable {
 
-	
+	private static final long serialVersionUID = 8494633931799962941L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "ws_mov_id")
+	private int id;
 	
 }
