cb9ee987387bb652632e1d9b71eaf2b497d031bc
jcall <jcall@DELL-JOSE>
2020-01-16 12:41:22 -0500

    Gestion y dao cuenta creado

--- PracticaWS/src/main/java/negocio/GestionCuenta.java
+++ PracticaWS/src/main/java/negocio/GestionCuenta.java
@@ -1,5 +1,31 @@
 package negocio;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import datos.CuentaDAO;
+import modelo.Cuenta;
+import modelo.Movimiento;
+
+@Stateless
 public class GestionCuenta {
+	
+	@Inject
+	private CuentaDAO dao;
+	
+	public void insertCuenta(int id, String nombre, String apellido, Double saldo) {
+		
+		Cuenta cuenta = new Cuenta();
+		cuenta.setId(id);
+		cuenta.setNombre(nombre);
+		cuenta.setApellido(apellido);
+		cuenta.setSaldo(saldo);
+		dao.insert(cuenta);
+		
+	}
+	
+
+	
 
 }
+
